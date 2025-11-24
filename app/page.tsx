ea1054e0b2b4c5c0a6bc0c08bed842fd9ce85c9e
nevinsebastian <heynevinhere@gmail.com>
2025-11-24 17:00:17 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import NavbarDemo from "@/components/ui/resizable-navbar-demo";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black">
+      <NavbarDemo />
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
